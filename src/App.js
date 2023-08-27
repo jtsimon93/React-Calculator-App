82687a98abb020008307760f082b4851f1ba762e
Justin Simon <jtsimon@jtsimon.dev>
2023-08-27 19:23:03 -0400

    Added footer, added favicon and touch icon

--- src/App.js
+++ src/App.js
@@ -147,44 +147,50 @@ function App() {
   );
 
   return (
-    <div className="calculator-grid">
-      <div className={"output"}>
-        <div className={"previous-operand"}>
-          {previousOperand} {operation}
+    <>
+      <div className="calculator-grid">
+        <div className={"output"}>
+          <div className={"previous-operand"}>
+            {previousOperand} {operation}
+          </div>
+          <div className={"current-operand"}>{currentOperand}</div>
         </div>
-        <div className={"current-operand"}>{currentOperand}</div>
+        <button
+          className={"span-two"}
+          onClick={() => dispatch({ type: ACTIONS.CLEAR })}
+        >
+          AC
+        </button>
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+          DEL
+        </button>
+        <OperationButton operation={"÷"} dispatch={dispatch} />
+        <DigitButton digit={"1"} dispatch={dispatch} />
+        <DigitButton digit={"2"} dispatch={dispatch} />
+        <DigitButton digit={"3"} dispatch={dispatch} />
+        <OperationButton operation={"*"} dispatch={dispatch} />
+        <DigitButton digit={"4"} dispatch={dispatch} />
+        <DigitButton digit={"5"} dispatch={dispatch} />
+        <DigitButton digit={"6"} dispatch={dispatch} />
+        <OperationButton operation={"+"} dispatch={dispatch} />
+        <DigitButton digit={"7"} dispatch={dispatch} />
+        <DigitButton digit={"8"} dispatch={dispatch} />
+        <DigitButton digit={"9"} dispatch={dispatch} />
+        <OperationButton operation={"-"} dispatch={dispatch} />
+        <DigitButton digit={"."} dispatch={dispatch} />
+        <DigitButton digit={"0"} dispatch={dispatch} />
+        <button
+          className={"span-two"}
+          onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+        >
+          =
+        </button>
       </div>
-      <button
-        className={"span-two"}
-        onClick={() => dispatch({ type: ACTIONS.CLEAR })}
-      >
-        AC
-      </button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
-        DEL
-      </button>
-      <OperationButton operation={"÷"} dispatch={dispatch} />
-      <DigitButton digit={"1"} dispatch={dispatch} />
-      <DigitButton digit={"2"} dispatch={dispatch} />
-      <DigitButton digit={"3"} dispatch={dispatch} />
-      <OperationButton operation={"*"} dispatch={dispatch} />
-      <DigitButton digit={"4"} dispatch={dispatch} />
-      <DigitButton digit={"5"} dispatch={dispatch} />
-      <DigitButton digit={"6"} dispatch={dispatch} />
-      <OperationButton operation={"+"} dispatch={dispatch} />
-      <DigitButton digit={"7"} dispatch={dispatch} />
-      <DigitButton digit={"8"} dispatch={dispatch} />
-      <DigitButton digit={"9"} dispatch={dispatch} />
-      <OperationButton operation={"-"} dispatch={dispatch} />
-      <DigitButton digit={"."} dispatch={dispatch} />
-      <DigitButton digit={"0"} dispatch={dispatch} />
-      <button
-        className={"span-two"}
-        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
-      >
-        =
-      </button>
-    </div>
+      <div className={"footer"}>
+        Created by <a href={"https://jtsimon.dev"}>Tanner Simon</a>. Open Source
+        Software, check it out on GitHub.
+      </div>
+    </>
   );
 }
 
